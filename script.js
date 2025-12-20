7a4873b845384ffeb9a464c61f459904695d963c
DanPorter1 <158497242+DanPorter1@users.noreply.github.com>
2025-12-20 21:40:18 +0000

    UI modification to searchbar - Change to JS inject

--- script.js
+++ script.js
@@ -3,15 +3,32 @@ const searchCheckbox = document.getElementById("search-check");
 const searchInputContainer = document.getElementById("search-input");
 const searchIcon = document.querySelector("#search-btn i");
 
+// searchIcon.addEventListener("click", () => {
+// searchCheckbox.checked = !searchCheckbox.checked;
+
+//   if (searchCheckbox.checked) {
+//     searchInputContainer.classList.add("visible");
+//   } else {
+//     
+//   }
+// });
+
+// REWORK SEARCH BOX -- ISSUES WITH TO-DO LINK 
 searchIcon.addEventListener("click", () => {
 searchCheckbox.checked = !searchCheckbox.checked;
 
   if (searchCheckbox.checked) {
+    searchInputContainer.innerHTML = `<input type="text" placeholder="Search..."> `
     searchInputContainer.classList.add("visible");
-  } else {
+    console.log("searchbar created");
+  }
+  else {
+    searchInputContainer.innerHTML = ""
     searchInputContainer.classList.remove("visible");
+    console.log("Removed Search");
   }
 });
+
 // Copy for menu button small media 
 const menuCheck = document.querySelector("#menu-btn input");
 const menuIcon = document.querySelector("#menu-btn i");
@@ -20,9 +37,11 @@ menuIcon.addEventListener("click", () => {
   menuCheck.checked = !menuCheck.checked;
 
   if (menuCheck.checked) {
+    searchInputContainer.innerHTML = `<input type="text" placeholder="Search..."> `
     searchInputContainer.classList.add("visible"); 
   } else {
     searchInputContainer.classList.remove("visible");
+    setTimeout(() => { searchInputContainer.innerHTML = ""; }, 320);
   }
 });
 // END OF SEARCH BOX TOGGLE
