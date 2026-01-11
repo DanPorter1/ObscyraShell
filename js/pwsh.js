41b52172f24da3236a54fe317a590785b817e654
DanPorter1 <158497242+DanPorter1@users.noreply.github.com>
2026-01-11 14:06:41 +0000

    Delete js directory

--- js/pwsh.js
+++
@@ -1,9 +0,0 @@
-// POWERSHELL
-const sections = document.querySelectorAll(".psection");
-
-sections.forEach(sec => {
-    sec.querySelector(".psection-head").addEventListener("click", () => {
-        sections.forEach(s => s !== sec && s.classList.remove("open"));
-        sec.classList.toggle("open");
-    });
-});
